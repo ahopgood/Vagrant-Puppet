5aac36119db74b83de7683dff638918f5919fc27
Alexander Hopgood <alex.hopgood@gmail.com>
2014-11-24 15:06:30 +0000

    Now disable iptables service so we can access tomcat's manager page.
    Setup vagrant file port forwarding from host 8081 to 8080 for the tomcat
    manager page.
    
    Added a tomcat-users.xml.erb template to create a manager user with
    customisable password, currently hardcoded in the module.
    
    Tomcat is started from the CATALINA_HOME/bin/startup.sh. Service startup
    to follow.
    
    Java is now specified as a required module at the beginning of the
    tomcat module.
    
    local_install_dir is now a parameter with a suitable default, this is to
    prevent clashes with the same java declaration.

--- modules/tomcat/manifests/init.pp
+++ modules/tomcat/manifests/init.pp
@@ -10,20 +10,25 @@
 #
 # Sample Usage:
 #
-class tomcat {
+
+class tomcat ($local_install_dir = '/etc/puppet/installers/') {
+require java
   notify {
-    'Tomcat installation':
+    "Tomcat installation test ${module_name}":
   }
   
   $vagrant_share      = "/installers/"
-  $local_install_path = "/etc/puppet/"
-  $local_install_dir  = "${local_install_path}installers/"
+#  $local_install_path = "/etc/puppet/"
+#  $local_install_dir  = "${local_install_path}installers/"
   $puppet_file_dir    = "modules/tomcat/"
   
   $tomcat_install_dir = "/var/hosting/"
   $tomcat_full_ver    = "apache-tomcat-7.0.54"
   $tomcat_short_ver   = "tomcat7"
-  $tomcat_file        = "${tomcat_full_ver}.tar.gz"
+  $tomcat_tar         = "${tomcat_full_ver}.tar"
+  $tomcat_file        = "${tomcat_tar}.gz"
+  $tomcat_users       = "${tomcat_install_dir}${tomcat_short_ver}/conf/tomcat-users.xml"
+
   $tomcat_installer   = "unpack_tomcat.sh"
   $tomcat_password    = "tomcat"
   
@@ -36,33 +41,37 @@ class tomcat {
   }
 
   #Create the directory we will install tomcat into.
+  #"${tomcat_install_dir}"
   file {
-    [ "${tomcat_install_dir}" ]:
+    [ "${tomcat_install_dir}"  ]:
     ensure    =>  directory,
-    mode      =>  0644,
+    mode      =>  0777,
     owner     =>  'tomcat',
+    require   =>  User['tomcat']
   }
   
   #Create the directory that the local install files will be copied to from the puppet fileserver or vagrant shared folder 
+  /*
   file {
     [ "${local_install_dir}" ]:
     ensure    =>  directory,
-    mode      =>  0644,
+    mode      =>  0777,
     owner     =>  'tomcat',
+    require   =>  User['tomcat']
   }
-
+ */
   file {
-    "${tomcat_file}":
-    path    =>  "${local_install_dir}${tomcat_file}",
+    [ "${tomcat_file}" ]:
+    path    =>  "${tomcat_install_dir}${tomcat_file}",
     ensure  =>  present,
     mode    =>  0777,
-    owner   =>  'tomcat',      
+    owner   =>  ['tomcat','vagrant'],      
     source  =>  ["puppet:///${puppet_file_dir}${tomcat_file}",
                     "${vagrant_share}${tomcat_file}"],
-    require =>  File["${local_install_dir}"],
+    require =>  File["${tomcat_install_dir}"],
   } 
   
-  /* 
+ /*
   file {
     "${tomcat_installer}":
     path    =>  "${local_install_dir}${$tomcat_installer}",
@@ -73,34 +82,32 @@ class tomcat {
                   "${vagrant_share}${tomcat_installer}"],
     require =>  File["${tomcat_file}"],
   }
-  
-
-  exec {
-    "Unpack tomcat archive":
-    path      =>  "${local_install_dir}",
-    command   =>  "${local_install_dir}${tomcat_installer}",
-    require   =>  File["${tomcat_installer}"],
-  }
 
   exec {
     "Uncompress tomcat archive":
     path      =>  "/bin/",
     command   =>  "gzip -d ${local_install_dir}${$tomcat_file}",
     user      =>  "tomcat",
+    require   => File["${tomcat_file}"],
   }
-*/
+  */
+
   exec {
     "Unpack tomcat archive":
     path      =>  "/bin/",
-    command   =>  "tar xfvz ${local_install_dir}${tomcat_file}",
+#    path      =>  "${local_install_dir}",
+    cwd       =>  "${tomcat_install_dir}",
+    command   =>  "/bin/tar xfvz ${tomcat_install_dir}${tomcat_file}",
     user      =>  "tomcat",
-    require   =>  File["${tomcat_file}"],
+    require   =>  File[ "${tomcat_file}" ],
   }
 
+#Fails on rerun if move has already happened, need to make it 
   exec {
     "Rename to tomcat7":
     path      =>  "/bin/",
-    command   =>  "mv ${local_install_dir}${tomcat_full_ver} ${tomcat_install_dir}${tomcat_short_ver}",
+    cwd       =>  "${tomcat_install_dir}",
+    command   =>  "cp -R ${tomcat_install_dir}${tomcat_full_ver} ${tomcat_short_ver}",
     user      =>  "tomcat",
     require   =>  Exec["Unpack tomcat archive"],
   }
@@ -119,12 +126,14 @@ class tomcat {
     user      =>  "tomcat",
   }
   */
+
   exec {
     "Set folder permissions":
     path      =>  "/bin/",
-    command   =>  " ${local_install_dir}${tomcat_short_ver}",
+    command   =>  "chmod -R 777 ${tomcat_install_dir}${tomcat_short_ver}",
+    require   =>  Exec["Rename to tomcat7"],
   }
-  
+   
   file {
     "Set JAVA_HOME":
     path      =>  "/etc/profile.d/java.sh",
@@ -138,7 +147,7 @@ class tomcat {
     content   =>  "export CATALINA_HOME=${$tomcat_install_dir}${tomcat_short_ver}",
     require   =>  File["Set JAVA_HOME"],
   }
-  
+/*  
   file  {
     "Logging Directory":
     path      =>  "${$tomcat_install_dir}${tomcat_short_ver}/logs",
@@ -148,14 +157,41 @@ class tomcat {
     recurse   =>   true,
     require   =>  File["Set CATALINA_HOME"],
   }
+*/
+  #If the service iptables is running then tomcat won't be accessible from a browser
+  service {
+    "iptables":
+    enable  =>  false,
+    ensure  => stopped,
+    require => File["Set CATALINA_HOME"]
+  }  
+
+  $tomcat_manager_password = "manager"
   
+  file {
+    "${tomcat_users}":
+    content => template('tomcat/tomcat-users.xml.erb'),
+    require =>  File["Set CATALINA_HOME"]
+  }
+
   exec {
     "Start tomcat":
     path      =>  "/usr/bin/",
     command   =>  "${$tomcat_install_dir}${tomcat_short_ver}/bin/startup.sh",
-    require   =>  File["Logging Directory"],
+    #require   =>  File["Logging Directory"],
+    require   =>  [File["Set CATALINA_HOME"],Service["iptables"],File["$tomcat_users"]],
     user      =>  "tomcat",
   }
+  
 
+  #Create a user template?
+  #Create a start up script
+  #Set the startup script as a service
+  
 }
-include tomcat
+
+#include java
+#include tomcat ('/etc/puppet/installers/')
+class { 'tomcat' :
+  local_install_dir => '/etc/puppet/installers/'
+}
