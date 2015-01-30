022c46f13a3529527bd4d0cc78e5e5261a955546
Alexander Hopgood <alex.hopgood@gmail.com>
2015-01-30 23:36:48 +0000

    Updated the master agent init.pp script.
    Added autosigning to the puppet.conf file.
    
    Added the puppet master startup script and set the chkconfig and set the
    service to run on start up.

--- master_agent/manifests/init.pp
+++ master_agent/manifests/init.pp
@@ -11,6 +11,12 @@
 # Sample Usage:
 #
 class master_agent {
+  $puppet_file_dir      = "modules/${module_name}/"
+  $puppetmaster         = "puppetmaster"
+  $puppet_conf          = "puppet.conf"
+  $vagrant_user         = "vagrant"
+  
+
   service { "iptables":
     enable  =>  false,
     ensure  => stopped,
@@ -29,18 +35,44 @@ class master_agent {
     owner     =>  "vagrant",
     group     =>  "vagrant",
   }
-  
-#  service { 'Start Puppet Master':
-#    ensure => running,
-#    enable => true,
-#  }
-  
-  exec { 'Start Masters':
-    path      =>  "/usr/bin/puppet",
-    cwd       =>  "/usr/bin/",
-    command   =>  "/usr/bin/puppet master",
-    require   =>  [Host['agent.vm'],Host['agentweb.vm'], File["Set puppet ownership"]]
+  #Need to add the following to the puppet.conf file:
+  #[master]
+  #autosign true
+  file {  "/etc/puppet/${puppet_conf}":
+    ensure  =>  present,
+    mode    =>  777,
+    owner   =>  "${vagrant_user}",
+    source  =>  ["/tmp/vagrant-puppet-3/files/files/${puppet_conf}", 
+      "puppet://${puppet_file_dir}/${puppet_conf}"]
   }
+  
+  #Startup script for the puppet master service
+  file { "${puppetmaster}" :
+    path    =>  "/etc/init.d/${puppetmaster}",
+    ensure  =>  present,
+    mode    =>  0777,
+    owner   =>  "${vagrant_user}",
+    source  =>  ["/tmp/vagrant-puppet-3/files/files/${puppetmaster}",
+      "puppet://${puppet_file_dir}/${puppetmaster}"],
+    notify  =>  Service["${puppetmaster}"],
+    require =>  File["/etc/puppet/${puppet_conf}"]
+  } 
+  
+  service { "${puppetmaster}" :
+    ensure  => running,
+    enable  => true,
+    require => File["${puppetmaster}"]
+  }
+  #Perform service start if: ps aux | grep puppet | grep -v grep
+  
+  #Need to 
+  
+#  exec { 'Start Masters':
+#    path      =>  "/usr/bin/puppet",
+#    cwd       =>  "/usr/bin/",
+#    command   =>  "/usr/bin/puppet master",
+#    require   =>  [Host['agent.vm'],Host['agentweb.vm'], File["Set puppet ownership"]]
+#  }
   
 }
 include master_agent
