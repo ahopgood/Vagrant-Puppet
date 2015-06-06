7e46487b0bb5ec81213196434a91bd15ca2ee760
Alexander Hopgood <alex.hopgood@gmail.com>
2015-06-06 18:35:35 +0100

    Ignored tomcat 7 gizpped archive.
    tomcat 6 and 8 manifests added.

---
+++ modules/tomcat/manifests/tomcat8.pp
@@ -0,0 +1,23 @@
+# Class: tomcat
+#
+# This module manages tomcat
+#
+# Parameters: none
+#
+# Actions:
+#
+# Requires: see Modulefile
+#
+# Sample Usage:
+#
+
+class { 'tomcat':
+	tomcat_manager_username =>  'admin',
+    tomcat_manager_password =>  'adminadmin',
+#    logging_directory =>  '/var/log/tomcat8',
+	major_version => '8',
+    minor_version => '23',
+    port => '8080',
+#    java_opts => "-Xms128m",
+    catalina_opts => "-Xms256m"
+}
