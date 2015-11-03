6d1fb15bbca1251f0fd25e85d5ffd91827962cf0
Alexander Hopgood <alex.hopgood@gmail.com>
2015-11-03 12:13:20 +0000

    Added httpd and php local dependencies, switched to rpm.
    Added httpd dependencies:
    * apr
    * apr-utils
    * apr-utils-ldap
    * mailcap
    * httpd-tools
    Added php dependencies:
    * php-common
    * php-cli

--- modules/kanboard/manifests/init.pp
+++ modules/kanboard/manifests/init.pp
@@ -19,137 +19,264 @@ class kanboard (
   $local_install_path = "/etc/puppet/"
   $local_install_dir = "${local_install_path}installers/"
   $puppet_file_dir = "modules/kanboard/"
-  $httpd_file = "httpd-2.2.15-47.el6.centos.x86_64.rpm"
-  $php_file = "php-5.3.3-46.el6_6.x86_64.rpm"
-  $php_gd_file = "php-gd-5.3.3-46.el6_6.x86_64.rpm"
-  $php_mbstring_file = "php-mbstring-5.3.3-46.el6_6.x86_64.rpm"
-  $php_mysql_file = "php-mysql-5.3.3-46.el6_6.x86_64.rpm"
-  $php_pdo_file = "php-pdo-5.3.3-46.el6_6.x86_64.rpm"
-  $unzip_file = "unzip-6.0-2.el6_6.x86_64.rpm"
+  
+ $unzip_file = "unzip-6.0-2.el6_6.x86_64.rpm"
   $wget_file = "wget-1.12-5.el6_6.1.x86_64.rpm"
   $patch_file = "patch-2.6-6.el6.x86_64.rpm"
  
-  
+  Class["mysql"] -> Class["kanboard"]
+  /*
   Package["httpd"] -> Package["php"] ->
   Package["php-mbstring"] -> Package["php-pdo"] ->
-  Package["php-gd"] -> Package["php-mysql"]-> 
+#  Package["php-gd"] -> Package["php-mysql"] ->
+  Package["php-gd"] -> 
   Package["unzip"] -> Package["wget"] ->
   Package["patch"] ->
   Exec["install"] -> Augeas["php.ini"]
-  
+  */
 #  file {
 #    "${local_install_dir}":
 #    path       =>  "${local_install_dir}",
 #    ensure     =>  directory,
-#  }
-    
+#  } 
+  $apr_file = "apr-1.3.9-5.el6_2.x86_64.rpm"
   file{
-    ["${local_install_dir}${httpd_file}", 
-    "${local_install_dir}${php_file}",
-    "${local_install_dir}${php_gd_file}",
-    "${local_install_dir}${php_mbstring_file}",
-    "${local_install_dir}${$php_mysql_file}",
-    "${local_install_dir}${php_pdo_file}",
-    "${local_install_dir}${unzip_file}",
-    "${local_install_dir}${wget_file}",
-    "${local_install_dir}${patch_file}"]:
-#    require => File["${local_install_dir}"],
-#    path => "${local_install_dir}",
+    "${local_install_dir}${apr_file}":
     ensure => present,
-    source => ["puppet:///${puppet_file_dir}${httpd_file}",
-      "puppet:///${puppet_file_dir}${php_file}",
-      "puppet:///${puppet_file_dir}${php_gd_file}",
-      "puppet:///${puppet_file_dir}${php_mbstring_file}",
-      "puppet:///${puppet_file_dir}${$php_mysql_file}",
-      "puppet:///${puppet_file_dir}${php_pdo_file}",
-      "puppet:///${puppet_file_dir}${unzip_file}",
-      "puppet:///${puppet_file_dir}${wget_file}",
-      "puppet:///${puppet_file_dir}${patch_file}"]
+    path => "${local_install_dir}${apr_file}",
+    source => ["puppet:///${puppet_file_dir}${apr_file}"]
   }
-   
+  package {"apr":
+    ensure => present,
+    provider => 'rpm',
+    source => "${local_install_dir}${apr_file}",
+    require => File["${local_install_dir}${apr_file}"]
+  }
+  $apr_utils_file = "apr-util-1.3.9-3.el6_0.1.x86_64.rpm"
+  file{
+    "${local_install_dir}${apr_utils_file}":
+    ensure => present,
+    path => "${local_install_dir}${apr_utils_file}",
+    source => ["puppet:///${puppet_file_dir}${apr_utils_file}"]
+  }
+  package {"apr-util":
+    ensure => present,
+    provider => 'rpm',
+    source => "${local_install_dir}${apr_utils_file}",
+    require => [File["${local_install_dir}${apr_utils_file}"], Package["apr"]]
+  }
+  $apr_utils_ldap_file = "apr-util-ldap-1.3.9-3.el6_0.1.x86_64.rpm"
+  file{
+    "${local_install_dir}${apr_utils_ldap_file}":
+    ensure => present,
+    path => "${local_install_dir}${apr_utils_ldap_file}",
+    source => ["puppet:///${puppet_file_dir}${apr_utils_ldap_file}"]
+  }
+  package {"apr-util-ldap":
+    ensure => present,
+    provider => 'rpm',
+    source => "${local_install_dir}${apr_utils_ldap_file}",
+    require => [File["${local_install_dir}${apr_utils_ldap_file}"], Package["apr"], Package["apr-util"]]
+  }
+  $mail_cap_file = "mailcap-2.1.31-2.el6.noarch.rpm"
+  file{
+    "${local_install_dir}${mail_cap_file}":
+    ensure => present,
+    path => "${local_install_dir}${mail_cap_file}",
+    source => ["puppet:///${puppet_file_dir}${mail_cap_file}"]
+  }
+  package {"mailcap":
+    ensure => present,
+    provider => 'rpm',
+    source => "${local_install_dir}${mail_cap_file}",
+    require => File["${local_install_dir}${mail_cap_file}"]
+  }
+  $httpd_tools_file = "httpd-tools-2.2.15-47.el6.centos.x86_64.rpm"
+  file{
+    "${local_install_dir}${httpd_tools_file}":
+    ensure => present,
+    path => "${local_install_dir}${httpd_tools_file}",
+    source => ["puppet:///${puppet_file_dir}${httpd_tools_file}"]
+  }
+  package {"httpd-tools":
+    ensure => present,
+    provider => 'rpm',
+    source => "${local_install_dir}${httpd_tools_file}",
+    require => [File["${local_install_dir}${httpd_tools_file}"],Package["apr-util"]]
+  }
+  $httpd_file = "httpd-2.2.15-47.el6.centos.x86_64.rpm"
+  file{
+    "${local_install_dir}${httpd_file}":
+    ensure => present,
+    path => "${local_install_dir}${httpd_file}",
+    source => ["puppet:///${puppet_file_dir}${httpd_file}",]
+  }
   package {"httpd":
     ensure => present, #will require the yum / puppet resource package name
-    provider => 'yum',
+    provider => 'rpm',
     source => "${local_install_dir}${httpd_file}",
-    require => File["${local_install_dir}${httpd_file}"],
+    require => [File["${local_install_dir}${httpd_file}"], Package["apr-util-ldap"], 
+    Package["mailcap"], Package["httpd-tools"], Package["apr-util"]],
     #version 2.2.15
+  }
+  service {
+    "httpd":
+    require => Package["httpd"],
+    ensure => running,
+    enable => true
+  }
+  
+  $php_file = "php-5.3.3-46.el6_6.x86_64.rpm"
+  $php_cli_file = "php-cli-5.3.3-46.el6_6.x86_64.rpm"
+  $php_common_file = "php-common-5.3.3-46.el6_6.x86_64.rpm"
+  $php_mbstring_file = "php-mbstring-5.3.3-46.el6_6.x86_64.rpm"
+  $php_pdo_file = "php-pdo-5.3.3-46.el6_6.x86_64.rpm"
+  $php_gd_file = "php-gd-5.3.3-46.el6_6.x86_64.rpm"
+  $php_mysql_file = "php-mysql-5.3.3-46.el6_6.x86_64.rpm"
+
+  file{
+    "${local_install_dir}${php_cli_file}":
+    ensure => present,
+    source => ["puppet:///${puppet_file_dir}${php_cli_file}",]
+  }  
+  package {"php-cli":
+    ensure => present,
+    provider => 'rpm',
+    source => "${local_install_dir}${php_cli_file}",
+    require => File["${local_install_dir}${php_cli_file}"],
+    #version 5.3.3
+  }  
+  file{
+    "${local_install_dir}${php_common_file}":
+    ensure => present,
+    source => ["puppet:///${puppet_file_dir}${php_common_file}",]
+  }
+  package {"php-common":
+    ensure => present,
+    provider => 'rpm',
+    source => "${local_install_dir}${php_common_file}",
+    require => File["${local_install_dir}${php_common_file}"],
+    #version 5.3.3
+  }
+  file{
+    "${local_install_dir}${php_file}":
+    ensure => present,
+    source => ["puppet:///${puppet_file_dir}${php_file}",]
   }
   package {"php":
     ensure => present,
-    provider => 'yum',
-    source => "${local_install_dir}${httpd_file}",
-    require => File["${local_install_dir}${httpd_file}"],
+    provider => 'rpm',
+    source => "${local_install_dir}${php_file}",
+    require => [File["${local_install_dir}${php_file}"], Package["php-common"], Package["php-cli"], Package["httpd"]],
     #version 5.3.3
+  }
+/* 
+  file{
+    "${local_install_dir}${php_mbstring_file}":
+    ensure => present,
+    source => ["puppet:///${puppet_file_dir}${php_mbstring_file}",]
   }
   package {"php-mbstring":
     ensure => present,
-    provider => 'yum',
-    source => "${local_install_dir}${httpd_file}",
-    require => File["${local_install_dir}${httpd_file}"],
+    provider => 'rpm',
+    source => "${local_install_dir}${php_mbstring_file}",
+    require => [File["${local_install_dir}${php_mbstring_file}"], Package["php-common"]],
   }  
+
+  file{
+    "${local_install_dir}${php_pdo_file}":
+    ensure => present,
+    source => "puppet:///${puppet_file_dir}${php_pdo_file}",
+  }
   package {"php-pdo":
     ensure => present,
-    provider => 'yum',
-    source => "${local_install_dir}${httpd_file}",
-    require => File["${local_install_dir}${httpd_file}"],
+    provider => 'rpm',
+    source => "${local_install_dir}${php_pdo_file}",
+    require => [File["${local_install_dir}${php_pdo_file}"],Package["php-common"]],
+  }
+
+  file{
+    "${local_install_dir}${php_gd_file}":
+    ensure => present,
+    source => "puppet:///${puppet_file_dir}${php_gd_file}",
   }
   package {"php-gd":
     ensure => present,
-    provider => 'yum',
-    source => "${local_install_dir}${httpd_file}",
-    require => File["${local_install_dir}${httpd_file}"],
+    provider => 'rpm',
+    source => "${local_install_dir}${php_gd_file}",
+    require => [File["${local_install_dir}${php_gd_file}"],Package["php-common"]],
     #2.0.34
+  } */
+/*
+  file{
+    "${local_install_dir}${php_mysql_file}":
+    ensure => present,
+    source => "puppet:///${puppet_file_dir}${php_mysql_file}",
   }
   package {"php-mysql":
     ensure => present,
-    provider => 'yum',
-    source => "${local_install_dir}${httpd_file}",
-    require => File["${local_install_dir}${httpd_file}"],
+    provider => 'rpm',
+    source => "${local_install_dir}${php_mysql_file}",
+    require => [File["${local_install_dir}${php_mysql_file}"], Class["mysql"]],
     #5.1.73
+  }
+ */
+/*
+  file{
+    "${local_install_dir}${unzip_file}":
+    ensure => present,
+    source => "puppet:///${puppet_file_dir}${unzip_file}",
   }
   package {"unzip":
     ensure => present,
-    provider => 'yum',
-    source => "${local_install_dir}${httpd_file}",
-    require => File["${local_install_dir}${httpd_file}"],
+    provider => 'rpm',
+    source => "${local_install_dir}${unzip_file}",
+    require => File["${local_install_dir}${unzip_file}"],
     #6.00
+  }
+
+  file{
+    "${local_install_dir}${wget_file}":
+    ensure => present,
+    source => "puppet:///${puppet_file_dir}${wget_file}",
   }
   package {"wget":
     ensure => present,
-    provider => 'yum',
-    source => "${local_install_dir}${httpd_file}",
-    require => File["${local_install_dir}${httpd_file}"],
+    provider => 'rpm',
+    source => "${local_install_dir}${wget_file}",
+    require => File["${local_install_dir}${wget_file}"],
     #1.12
+  }
+
+  file{
+    "${local_install_dir}${patch_file}":
+    ensure => present,
+    source => "puppet:///${puppet_file_dir}${patch_file}",
   }
   package {"patch":
     ensure => present,
-    provider => 'yum',
+    provider => 'rpm',
     source => "${local_install_dir}${patch_file}",
     require => File["${local_install_dir}${patch_file}"],
     #1.12
   }
+
+  #Installers
   file{"install.sh":
     ensure => present,
     path => "/usr/local/bin/install.sh",
-    source => ["puppet:///${puppet_file_dir}install.sh"]
+    source => ["puppet:///${puppet_file_dir}install.sh"],
   }
 
   Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
   
   exec{
     "install":
-    require => Class["mysql"],
+    require => [Class["mysql"],File["install.sh"],Package["httpd"]],
     command => "/usr/local/bin/install.sh",
     cwd => "/home/vagrant",
-    notify => Service["httpd"]
   }
-  
-  service {
-    "httpd":
-    ensure => running,
-    enable => true
-  }
-  
+    
   augeas { 
     "php.ini":
     context => "/files/etc/php.ini/PHP/",
@@ -188,7 +315,7 @@ class kanboard (
 #  }
   
   file{"config.php":
-#    require => Exec["Update_db_table"],
+    require => Exec["install"],
     path => "/var/www/html/kanboard/config.php",
     content => template("${module_name}/config.default.php.erb"),
     ensure => present,
@@ -204,7 +331,7 @@ class kanboard (
 #    path => "/usr/bin/", 
 #  }
   
-
+ */
   notify {
     "Kanboard":
   }
