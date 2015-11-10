789c6ea72ec234a3beca57ca94944749787dc712
Alexander Hopgood <alex.hopgood@gmail.com>
2015-11-10 15:34:48 +0000

    Removed mysql call from wordpress install.sh.
    Added the mysql module as a resource dependency instead.

--- modules/wordpress/manifests/init.pp
+++ modules/wordpress/manifests/init.pp
@@ -10,6 +10,18 @@
 #
 # Sample Usage:
 #
-class wordpress {
-
+class wordpress (
+  $major_version = '4',
+  $minor_version = '3',
+  $patch_version = '1'
+) {
+  Class["mysql"] -> Class["kanboard"]
+  #Set firewall port in iptables
+  #
+  #Install mysql
+  #Setup wordpress database
+  #Setup root user and password on mysql
+  #
+  #Unzipping of wordpres-4.3.1.tar.gz into html shared folder
+  
 }
