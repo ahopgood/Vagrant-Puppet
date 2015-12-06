eceee8dc5eda830cd9c01ae781315a75b87189d1
Alexander Hopgood <alex.hopgood@gmail.com>
2015-12-06 18:35:08 +0000

    Modified the Kanboard module to use the Class['http'] instead of inplace file and package declarations.

--- modules/kanboard/manifests/init.pp
+++ modules/kanboard/manifests/init.pp
@@ -23,9 +23,15 @@ class kanboard (
   #$puppet_file_dir = "modules/kanboard/"
   $puppet_file_dir = "modules/${module_name}/"
    
-  Class["mysql"] -> 
-  #Class["httpd"] -> 
-  Class["kanboard"]
+  notify {
+    "${module_name} installation completed":
+  }  
+
+
+  Class["mysql"] 
+  -> 
+  Class["httpd"]
+  #Class["kanboard"]
   /*
   Package["httpd"] -> Package["php"] ->
   Package["php-mbstring"] -> Package["php-pdo"] ->
@@ -40,124 +46,6 @@ class kanboard (
 #    path       =>  "${local_install_dir}",
 #    ensure     =>  directory,
 #  }
-  
-  $apr_file = "apr-1.3.9-5.el6_2.x86_64.rpm"
-  file{
-    "${local_install_dir}${apr_file}":
-    ensure => present,
-    path => "${local_install_dir}${apr_file}",
-    source => ["puppet:///${puppet_file_dir}${apr_file}"]
-  }
-  package {"apr":
-    ensure => present,
-    provider => 'rpm',
-    source => "${local_install_dir}${apr_file}",
-    require => File["${local_install_dir}${apr_file}"]
-  }
-  $apr_utils_file = "apr-util-1.3.9-3.el6_0.1.x86_64.rpm"
-  file{
-    "${local_install_dir}${apr_utils_file}":
-    ensure => present,
-    path => "${local_install_dir}${apr_utils_file}",
-    source => ["puppet:///${puppet_file_dir}${apr_utils_file}"]
-  }
-  package {"apr-util":
-    ensure => present,
-    provider => 'rpm',
-    source => "${local_install_dir}${apr_utils_file}",
-    require => [File["${local_install_dir}${apr_utils_file}"], Package["apr"]]
-  }
-  $apr_utils_ldap_file = "apr-util-ldap-1.3.9-3.el6_0.1.x86_64.rpm"
-  file{
-    "${local_install_dir}${apr_utils_ldap_file}":
-    ensure => present,
-    path => "${local_install_dir}${apr_utils_ldap_file}",
-    source => ["puppet:///${puppet_file_dir}${apr_utils_ldap_file}"]
-  }
-  package {"apr-util-ldap":
-    ensure => present,
-    provider => 'rpm',
-    source => "${local_install_dir}${apr_utils_ldap_file}",
-    require => [File["${local_install_dir}${apr_utils_ldap_file}"], Package["apr"], Package["apr-util"]]
-  }
-  $mail_cap_file = "mailcap-2.1.31-2.el6.noarch.rpm"
-  file{
-    "${local_install_dir}${mail_cap_file}":
-    ensure => present,
-    path => "${local_install_dir}${mail_cap_file}",
-    source => ["puppet:///${puppet_file_dir}${mail_cap_file}"]
-  }
-  package {"mailcap":
-    ensure => present,
-    provider => 'rpm',
-    source => "${local_install_dir}${mail_cap_file}",
-    require => File["${local_install_dir}${mail_cap_file}"]
-  }
-  $httpd_tools_file = "httpd-tools-2.2.15-47.el6.centos.x86_64.rpm"
-  file{
-    "${local_install_dir}${httpd_tools_file}":
-    ensure => present,
-    path => "${local_install_dir}${httpd_tools_file}",
-    source => ["puppet:///${puppet_file_dir}${httpd_tools_file}"]
-  }
-  package {"httpd-tools":
-    ensure => present,
-    provider => 'rpm',
-    source => "${local_install_dir}${httpd_tools_file}",
-    require => [File["${local_install_dir}${httpd_tools_file}"],Package["apr-util"]]
-  }
-  $httpd_file = "httpd-2.2.15-47.el6.centos.x86_64.rpm"
-  file{
-    "${local_install_dir}${httpd_file}":
-    ensure => present,
-    path => "${local_install_dir}${httpd_file}",
-    source => ["puppet:///${puppet_file_dir}${httpd_file}",]
-  }
-  package {"httpd":
-    ensure => present, #will require the yum / puppet resource package name
-    provider => 'rpm',
-    source => "${local_install_dir}${httpd_file}",
-    require => [File["${local_install_dir}${httpd_file}"], Package["apr-util-ldap"], 
-    Package["mailcap"], Package["httpd-tools"], Package["apr-util"]],
-    #version 2.2.15
-  }
-  service {
-    "httpd":
-    require => Package["httpd"],
-    ensure => running,
-    enable => true
-  }
-
-  $freetype_file = "freetype-2.3.11-15.el6_6.1.x86_64.rpm"
-  file{
-    "${local_install_dir}${$freetype_file}":
-    ensure => present,
-    source => ["puppet:///${puppet_file_dir}${freetype_file}",]
-  }  
-  package {"freetype":
-    ensure => present,
-    provider => 'rpm',
-    source => "${local_install_dir}${freetype_file}",
-    require => File["${local_install_dir}${freetype_file}"],
-    #version 5.3.3
-  }
-  
-  $libxpm_file = "libXpm-3.5.10-2.el6.x86_64.rpm"
-  file{
-    "${local_install_dir}${libxpm_file}":
-    ensure => present,
-    source => ["puppet:///${puppet_file_dir}${libxpm_file}",]
-  }
-  package {"libXpm":
-    ensure => present,
-    provider => 'rpm',
-    source => "${local_install_dir}${libxpm_file}",
-#    source => ["puppet:///${puppet_file_dir}${libxpm_file}",]
-    require => File["${local_install_dir}${libxpm_file}"],
-    #version 5.3.3
-  }
-/**/
-
     
   $php_file = "php-5.3.3-46.el6_6.x86_64.rpm"
   $php_cli_file = "php-cli-5.3.3-46.el6_6.x86_64.rpm"
--- modules/kanboard/manifests/kanboard.pp
+++ modules/kanboard/manifests/kanboard.pp
@@ -1,10 +1,19 @@
 Package{
   allow_virtual => false
 }
+  $local_install_path = "/etc/puppet/"
+  $local_install_dir = "${local_install_path}installers/"
+
+#  file {
+#    "${local_install_dir}":
+#    path       =>  "${local_install_dir}",
+#    ensure     =>  directory,
+#  } 
+
 
 class {'mysql':}
-
-#class {'httpd':}
+->
+class {'httpd':}
 
 class {'kanboard':
   backup_path => "/vagrant/backups/"
