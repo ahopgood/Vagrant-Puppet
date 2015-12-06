7ae1558aa3dbc6016119ed8e36beb12bb75785eb
Alexander Hopgood <alex.hopgood@gmail.com>
2015-12-06 18:29:36 +0000

    Created puppet manifest for apache httpd.

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -13,13 +13,14 @@
 class httpd {
 
   $puppet_file_dir = "modules/httpd/"
+  #$puppet_file_dir = "modules/${module_name}"
   
 #  Package["httpd"] -> Package["httpd-tools"] -> Package["mailcap"] ->
 #  Package["apr-util-ldap"] -> Package["apr-util"] -> Package["apr"] 
 
   $apr_file = "apr-1.3.9-5.el6_2.x86_64.rpm"
   file{
-    "${apr_file}":
+    "${local_install_dir}${apr_file}":
     ensure => present,
     path => "${local_install_dir}${apr_file}",
     source => ["puppet:///${puppet_file_dir}${apr_file}"]
@@ -28,11 +29,11 @@ class httpd {
     ensure => present,
     provider => 'rpm',
     source => "${local_install_dir}${apr_file}",
-    require => File["${apr_file}"]
+    require => File["${local_install_dir}${apr_file}"]
   }
   $apr_utils_file = "apr-util-1.3.9-3.el6_0.1.x86_64.rpm"
   file{
-    "${apr_utils_file}":
+    "${local_install_dir}${apr_utils_file}":
     ensure => present,
     path => "${local_install_dir}${apr_utils_file}",
     source => ["puppet:///${puppet_file_dir}${apr_utils_file}"]
@@ -41,11 +42,11 @@ class httpd {
     ensure => present,
     provider => 'rpm',
     source => "${local_install_dir}${apr_utils_file}",
-    require => [File["${apr_utils_file}"], Package["apr"]]
+    require => [File["${local_install_dir}${apr_utils_file}"], Package["apr"]]
   }
   $apr_utils_ldap_file = "apr-util-ldap-1.3.9-3.el6_0.1.x86_64.rpm"
   file{
-    "${apr_utils_ldap_file}":
+    "${local_install_dir}${apr_utils_ldap_file}":
     ensure => present,
     path => "${local_install_dir}${apr_utils_ldap_file}",
     source => ["puppet:///${puppet_file_dir}${apr_utils_ldap_file}"]
@@ -54,11 +55,11 @@ class httpd {
     ensure => present,
     provider => 'rpm',
     source => "${local_install_dir}${apr_utils_ldap_file}",
-    require => [File["${apr_utils_ldap_file}"], Package["apr"], Package["apr-util"]]
+    require => [File["${local_install_dir}${apr_utils_ldap_file}"], Package["apr"], Package["apr-util"]]
   }
   $mail_cap_file = "mailcap-2.1.31-2.el6.noarch.rpm"
   file{
-    "${mail_cap_file}":
+    "${local_install_dir}${mail_cap_file}":
     ensure => present,
     path => "${local_install_dir}${mail_cap_file}",
     source => ["puppet:///${puppet_file_dir}${mail_cap_file}"]
@@ -67,11 +68,11 @@ class httpd {
     ensure => present,
     provider => 'rpm',
     source => "${local_install_dir}${mail_cap_file}",
-    require => File["${mail_cap_file}"]
+    require => File["${local_install_dir}${mail_cap_file}"]
   }
   $httpd_tools_file = "httpd-tools-2.2.15-47.el6.centos.x86_64.rpm"
   file{
-    "${httpd_tools_file}":
+    "${local_install_dir}${httpd_tools_file}":
     ensure => present,
     path => "${local_install_dir}${httpd_tools_file}",
     source => ["puppet:///${puppet_file_dir}${httpd_tools_file}"]
@@ -80,11 +81,11 @@ class httpd {
     ensure => present,
     provider => 'rpm',
     source => "${local_install_dir}${httpd_tools_file}",
-    require => File["${httpd_tools_file}"]
+    require => [File["${local_install_dir}${httpd_tools_file}"],Package["apr-util"]]
   }
   $httpd_file = "httpd-2.2.15-47.el6.centos.x86_64.rpm"
   file{
-    "${httpd_file}":
+    "${local_install_dir}${httpd_file}":
     ensure => present,
     path => "${local_install_dir}${httpd_file}",
     source => ["puppet:///${puppet_file_dir}${httpd_file}",]
@@ -93,7 +94,7 @@ class httpd {
     ensure => present, #will require the yum / puppet resource package name
     provider => 'rpm',
     source => "${local_install_dir}${httpd_file}",
-    require => [File["${httpd_file}"], Package["apr-util-ldap"], 
+    require => [File["${local_install_dir}${httpd_file}"], Package["apr-util-ldap"], 
     Package["mailcap"], Package["httpd-tools"], Package["apr-util"]],
     #version 2.2.15
   }
@@ -102,5 +103,34 @@ class httpd {
     require => Package["httpd"],
     ensure => running,
     enable => true
+  }
+
+  $freetype_file = "freetype-2.3.11-15.el6_6.1.x86_64.rpm"
+  file{
+    "${local_install_dir}${$freetype_file}":
+    ensure => present,
+    source => ["puppet:///${puppet_file_dir}${freetype_file}",]
+  }  
+  package {"freetype":
+    ensure => present,
+    provider => 'rpm',
+    source => "${local_install_dir}${freetype_file}",
+    require => File["${local_install_dir}${freetype_file}"],
+    #version 5.3.3
+  }
+  
+  $libxpm_file = "libXpm-3.5.10-2.el6.x86_64.rpm"
+  file{
+    "${local_install_dir}${libxpm_file}":
+    ensure => present,
+    source => ["puppet:///${puppet_file_dir}${libxpm_file}",]
+  }
+  package {"libXpm":
+    ensure => present,
+    provider => 'rpm',
+    source => "${local_install_dir}${libxpm_file}",
+#    source => ["puppet:///${puppet_file_dir}${libxpm_file}",]
+    require => File["${local_install_dir}${libxpm_file}"],
+    #version 5.3.3
   }
 }
