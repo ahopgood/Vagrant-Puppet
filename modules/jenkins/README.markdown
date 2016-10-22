394d546adec2e4886c3527d86c23bd39d73833b3
ahopgood <alex.hopgood@gmail.com>
2016-10-22 13:30:21 +0100

    Initial Jenkins module for Ubuntu Wily, currently supports LTS version 2.19 of Jenkins.

--- modules/jenkins/README.markdown
+++ modules/jenkins/README.markdown
@@ -1,3 +1,5 @@
 # jenkins #
 
 This is the jenkins module. It provides...
+
+Currently only supported on Ubuntu Wily (15.10) using version LTS version 2.19 of Jenkins.  
--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -14,19 +14,38 @@ class jenkins (
   $major_version = "1",
   $minor_version = "614" ) {
 
-  $jenkins_short_ver     = "jenkins${minor_version}"
+  $jenkins_short_ver     = "jenkins"
 
   $jenkins_group         = "${jenkins_short_ver}"
   $jenkins_user          = "${jenkins_short_ver}"
 
+  $local_install_path = "/etc/puppet/"
+  $local_install_dir  = "${local_install_path}installers/"
+  $puppet_file_dir    = "modules/jenkins/"
+  
   notify {
     "${module_name} installation completed":
   }  
 
+  $java_major_version = "8"
+  $java_update_version = "112"
+
+#Ubuntu
+#15.10 = wiley
+#16.04 = Xenial
   if $::operatingsystem == 'CentOS' {
     notify {    "Using operating system:$::operatingsystem": }
+  } elsif $::operatingsystem == "Ubuntu"{
+    #create name of java deb file
+    $java_name = "oracle-java${java_major_version}-jdk_${java_major_version}u${java_update_version}_amd64-${operatingsystem}_${::operatingsystemmajrelease}.deb"
+    #Java deb format example oracle_java8-jdk_8u112_amd64-Ubuntu_15.10.deb
+    if $::operatingsystemmajrelease == "15.10" {
+      notify{"We're on Ubuntu wiley trying to use Java package ${java_name}":}
+    }
+    #install correct versions of dependencies for the ubuntu distro
+    
   } else {
-    notify {  "Operating system not supported:$::operatingsystem":  }  
+    notify {  "Operating system not supported:$::operatingsystem$::operatingsystemmajrelease":  }  
   }
 
    group { "${jenkins_group}":
@@ -47,13 +66,44 @@ class jenkins (
   #Requires java to be installed
   class { 'java': 
     version => '7',
-    updateVersion => '71',
+    updateVersion => '76',
+  }
+  
+  $daemon = "daemon_0.6.4-1_amd64.deb"
+  file {
+    "${daemon}":
+    require    =>  File["${local_install_dir}"],
+    path       =>  "${local_install_dir}${daemon}",
+    ensure     => present,
+    source     =>  ["puppet:///${puppet_file_dir}${daemon}"]
+  }
+  
+  package {
+    "${daemon}":
+    provider => "dpkg",
+    source => "${local_install_dir}${daemon}",
+    require => File["${daemon}"]
+  }
+  
+  $jenkins = "jenkins_2.19.1_all.deb"
+  file {
+    "${jenkins}":
+    require    =>  File["${local_install_dir}"],
+    path       =>  "${local_install_dir}${jenkins}",
+    ensure     => present,
+    source     =>  ["puppet:///${puppet_file_dir}${jenkins}"]
+  }
+  
+  package {
+    "${jenkins}":
+    provider => "dpkg",
+    source => "${local_install_dir}${jenkins}",
+    require => [File["${jenkins}"], Package["${daemon}"]]
   }
   
   #Install Jenkins
   #sudo rpm -ivh /vagrant/files/jenkins-1.614-1.1.noarch.rpm
   
   #iptables port exemption needed
-  )
   
 }
