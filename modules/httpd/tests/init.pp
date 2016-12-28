50a591f94fd2cec09c5d6a619ffbbac41c1188e9
ahopgood <alex.hopgood@gmail.com>
2016-12-28 11:57:13 +0000

    Updated Apache HTTP module readme
    Test scenarios also added

--- modules/httpd/tests/init.pp
+++ modules/httpd/tests/init.pp
@@ -1,1 +1,18 @@
 include httpd
+
+/* Test Scenarios */
+/*
+ * CentOS6 manifest run via vagrant VM
+ */
+
+/*
+ * CentOS7 manifest run via vagrant VM
+ */
+
+/*
+ * CentOS7 manifest run via vagrant VM
+ */
+
+/*
+ * Navigate to the IP address from the vagrant file, ensure that we get a 200 OK from apache
+ */
