9382a674c3429629815ff4bea4428e5df9c6e02b
ahopgood <alex.hopgood@gmail.com>
2016-12-30 11:05:05 +0000

    Created ufw module that allows for multiple usages to allow multiple ports to be opened.

---
+++ modules/ufw/manifests/init.pp
@@ -0,0 +1,44 @@
+# Class: ufw
+#
+# This module manages ufw
+#
+# Parameters: none
+#
+# Actions:
+#
+# Requires: see Modulefile
+#
+# Sample Usage:
+#
+define ufw (
+  $port = undef,
+  $isTCP = undef,
+) {
+  if ("${operatingsystem}" != "Ubuntu"){
+    fail("Operating System not supported ${operatingsystem}")
+  }
+  
+  if ("${isTCP}" == "true"){
+    $protocol = "tcp"
+  } else {
+    $protocol = "udp"
+  }
+
+  exec {"add firewall rule ${port}/${protocol}":
+    path => "/usr/sbin/",
+    command => "ufw allow ${port}/${protocol}",
+    unless => "ufw status | /bin/grep ${port}/${protocol} * ALLOW"
+  }
+  ->
+  exec {"Enable Firewall":
+    path => "/usr/sbin/",
+    command => "ufw --force enable",
+    onlyif => "ufw status | /bin/grep inactive",
+    notify => Service["ufw"]
+  }
+
+  service {"ufw":
+    ensure => running,
+    enable => true
+  }
+}
