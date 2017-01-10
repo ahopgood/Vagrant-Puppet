5a1c3ce292fa1febd5768aec5e356c260a5c53be
ahopgood <alex.hopgood@gmail.com>
2017-01-10 22:05:20 +0000

    New vagrant profile for ubuntu jekyll and new manifest with instructions for downloading and installing jekyll gems and dependencies offline.

---
+++ modules/jekyll/manifests/gem_jekyll.pp
@@ -0,0 +1,9 @@
+class gem_jekyll{
+  #gem install -i tmp jekyll -v 3.3.1
+  #tmp/cache/*.gem
+  #gem install -f --local /vagrant/files/*.gem
+  
+  #ubuntu/wily (15.10)
+  #Ruby version 2.1.5p273
+  #
+}
