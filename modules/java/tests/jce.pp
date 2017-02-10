0f8dfbfbda00971ce62f5eab1bb1cb9e3d863a17
Alexander Hopgood <alex.hopgood@gmail.com>
2017-02-10 11:14:38 +0000

    Updated the java::jce to allow for multiple calls, one for each major version of Java.
    Added variable $unpacked_jce_folder that reflects the internal folder naming of the jce-policy files for each major java version as they differ for each version.

--- modules/java/tests/jce.pp
+++ modules/java/tests/jce.pp
@@ -5,9 +5,32 @@ Package{
 java {"java-6":
   major_version => "6",
   update_version => "45",
+  multiTenancy => true,
 }
 ->
 java::jce {"jce":
   major_version => "6",
   update_version => "45",
+}
+->
+java {"java-7":
+  major_version => "7",
+  update_version => "76",
+  multiTenancy => true,
+}
+->
+java::jce {"jce7":
+  major_version => "7",
+  update_version => "76",
+}
+->
+java {"java-8":
+  major_version => "8",
+  update_version => "31",
+  multiTenancy => true,
+}
+->
+java::jce {"jce8":
+  major_version => "8",
+  update_version => "31",
 }
