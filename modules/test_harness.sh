8f609089e52e5af2d383f1228df3daa7c4ec0546
Alexander Hopgood <alex.hopgood@gmail.com>
2017-02-22 11:54:24 +0000

    Update test_harness documentation & improved logging (with colours!), also fixed an issue with the os_profile selection where it was being ignored.

--- modules/test_harness.sh
+++ modules/test_harness.sh
@@ -12,43 +12,39 @@ function run_manifest {
     snapshot_name=$4
     iteration=$5
 
+    RUN_MANIFEST_PREFIX=$PREFIX"\e[32mrun_manifest: \e[39m"
 
-    echo "Working on VM ["$vm_name"] with snapshot ["$snapshot_name"] and testing with manifest "$manifest_name
+    echo -e $RUN_MANIFEST_PREFIX"Working on VM ["$vm_name"] with snapshot ["$snapshot_name"] and testing with manifest "$manifest_name
 #    /usr/bin/ssh -p$port vagrant@localhost -i ~/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no 'sudo puppet apply /vagrant/tests/'$manifest_name' --noop --detailed-exitcodes' 2> $manifest_name"_"$vm_name"-errors.txt"
     /usr/bin/ssh -p$port vagrant@localhost -i ~/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no 'sudo puppet apply /vagrant/tests/'$manifest_name'' 2> $manifest_name"_"$vm_name"-errors.txt"
     RESULT=$?
     # Save result to global array
-    echo "Renaming error file to reflect exit code "$manifest_name"_"$vm_name"-errors-"$RESULT".txt"
-    mv $manifest_name"_"$vm_name"-errors.txt" $manifest_name"_"$vm_name"-errors-"$RESULT".txt"
+    OUTPUT_FILE=$manifest_name"_"$vm_name"-errors-"$RESULT".txt"
+    echo -e $RUN_MANIFEST_PREFIX"Renaming error file to reflect exit code "$OUTPUT_FILE
+    mv $manifest_name"_"$vm_name"-errors.txt" $OUTPUT_FILE
     if [ $RESULT == 0 ];then
-        echo "Removing empty error file "$manifest_name"_"$vm_name"-errors-"$RESULT".txt"
-        rm $manifest_name"_"$vm_name"-errors-"$RESULT".txt"
+        echo -e $RUN_MANIFEST_PREFIX"Removing empty error file "$OUTPUT_FILE
+        rm $OUTPUT_FILE
     fi
     RESULTS_ARRAY[$iteration]=$manifest_name" "$vm_name" result ["$RESULT"]"
-    echo "Result in array "${RESULTS_ARRAY[$iteration]}
+    echo -e $RUN_MANIFEST_PREFIX"Result in array "${RESULTS_ARRAY[$iteration]}
 }
 
 # Brings up vagrant box for specified profile, figures out the port, creates snapshot, iterates through list of manifests in the tests/ folder
 function run_vm {
+RUN_VM_PREFIX=$PREFIX"\e[36mrun_vm: \e[39m"
 # Add input checking for args 1 & 2
     vm_name=$1
     vm_iterator=$2
     # Get VM Name
-    snapshot_name="virgin"
-    echo "Creating VM ["$vm_name"]"
+    snapshot_name="blank"
+    echo -e $RUN_VM_PREFIX"Creating VM ["$vm_name"]"
     #Could add 1> /dev/null to sink output from vagrant elsewhere?
     vagrant up $vm_name
-
-    #Bail out here if we get a failure
-#    if [$?==0]:
-#    exit $?;
-
     #Capture SSH port to use
     port=$(vagrant ssh-config $vm_name | grep Port | awk '{ print $2 }')
-
     #Just in case the port numbers have changed per server then the saved key signature will be wrong so purge
     ssh-keygen -f $HOME"/.ssh/known_hosts" -R [localhost]:$port
-
     #Take snapshot so we can work on a fresh OS per puppet manifest we apply
     vagrant snapshot save $vm_name virgin
 
@@ -58,38 +54,41 @@ function run_vm {
     else
         MANIFESTS=($(ls -m tests | tr "," " "))
     fi
+    echo -e $RUN_VM_PREFIX"Using manifests ${MANIFESTS[@]}"
     #Create a count based on the vm number / iterator multiplied by the total number of manifests we'll run so we can insert them in the results array correctly
     iterator=$((${#MANIFESTS[*]} * $vm_iterator))
 
     for ((i=0; i < "${#MANIFESTS[*]}"; i++));
     do
+#        if [ ! -z SNAPSHOT ]; then
 #        vagrant up $vm_name
 #        port=$(vagrant ssh-config $vm_name | grep Port | awk '{ print $2 }')
 #        ssh-keygen -f $HOME"/.ssh/known_hosts" -R [localhost]:$port
-        echo "Manifest ${MANIFESTS[i]}"
+        echo -e $RUN_VM_PREFIX"Manifest ${MANIFESTS[i]}"
         run_manifest $port ${MANIFESTS[i]} $vm_name $snapshot_name $(($iterator + i))
 
         #Reset the snapshot
-        echo "Restoring snapshot ["$snapshot_name"] on VM ["$vm_name"] after test run result ["${RESULTS_ARRAY[$(($iterator + i))]}"]"
-        vagrant snapshot restore $vm_name virgin
-        #vagrant destroy $vm_name -f
+        echo -e $RUN_VM_PREFIX"Restoring snapshot ["$snapshot_name"] on VM ["$vm_name"] after test run result ["${RESULTS_ARRAY[$(($iterator + i))]}"]"
+#        vagrant snapshot restore $vm_name virgin
+#        vagrant destroy $vm_name -f
     done
 
     #Force vagrant to destroy the machine
-    vagrant destroy $vm_name -f
+   vagrant destroy $vm_name -f
 }
 
 # Enters the specified module folder and runs the vm for the Vagrant file contained there
 function run_module {
+    RUN_PREFIX=$PREFIX"\e[35m"$FUNCNAME": \e[39m"
     local OPTIND FLAG a
     while getopts m:p:t: FLAG; do
         case $FLAG in
             m)
-                echo "Using module path ["$OPTARG"]"
+                echo -e $RUN_PREFIX"Using module path ["$OPTARG"]"
                 MODULE=($OPTARG)
             ;;
             p)
-                echo "Using the vagrant profile ["$OPTARG"]"
+                echo -e $RUN_PREFIX"Using the vagrant profile ["$OPTARG"]"
                 if [ -z $OPTARG ]; then
                     VMs=""
                 else
@@ -97,58 +96,56 @@ function run_module {
                 fi
             ;;
             t)
-                echo "Using test manifests ["$OPTARG"]"
+                echo -e $RUN_PREFIX"Using test manifests ["$OPTARG"]"
                 if [ -z $OPTARG ]; then
                     TEST_MANIFESTS=""
                 else
                     TEST_MANIFESTS=$OPTARG
                 fi
-
-
             ;;
             \?)
-                echo "Unsupported parameter :$OPTARG"
+                echo -e $RUN_PREFIX"Unsupported parameter :$OPTARG"
             ;;
         esac
     done
 
     ORIGINAL_DIR=$(pwd)
     cd $MODULE
-    echo "In run_module for $MODULE... "$(pwd)
-    VMs=($(vagrant status | grep -E "*test" | awk '{ print $1 }'))
-#    if [ ! -z $2 ];then
-#        VMs=($2)
-#        echo "Using specific vagrant profile $2"
-#    fi
+    echo -e $RUN_PREFIX"for $MODULE... "$(pwd)
+    if [ -z $VMs ];then
+        VMs=($(vagrant status | grep -E "*test" | awk '{ print $1 }'))
+    fi
     #Handle vagrant not being installed
-    echo "Using Vagrant Profiles:"${VMs[@]}
+    echo -e $RUN_PREFIX"Using Vagrant Profiles:"${VMs[@]}
     for ((j=0; j < "${#VMs[*]}"; j++));
     do
-        echo "Starting run for "${VMs[j]}
+        echo -e $RUN_PREFIX"Starting run for "${VMs[j]}
         run_vm ${VMs[j]} j $TEST_MANIFESTS
     done
     cd $ORIGINAL_DIR
     #Need to move out of the current directory
-    echo "Leaving module $MODULE..."
+    echo -e $RUN_PREFIX"Leaving module $MODULE..."
 }
 
 declare -a RESULTS_ARRAY
-echo "Working in vagrant directory "$(pwd)
-#cd $1
-#echo "Using module directory $1"
+PREFIX="\e[34m$0: \e[39m"
+echo -e $PREFIX"Working in directory "$(pwd)
 
 VAGRANT_PROFILE=""
 TEST_MANIFESTS=""
-MODULES=($(ls -m | tr "," " "))
-
+MODULES=""
 while getopts m:p:t: FLAG; do
     case $FLAG in
         m)
-            echo "Setting module path to ["$OPTARG"]"
-            MODULES=($OPTARG)
+            if [ -z $OPTARG ]; then
+                MODULES=($(ls -m | tr "," " "))
+            else
+                MODULES=($OPTARG)
+            fi
+            echo -e $PREFIX"Setting module path to ["$MODULES"]"
         ;;
         p)
-            echo "Setting the vagrant profile to ["$OPTARG"]"
+            echo -e $PREFIX"Setting the vagrant profile to ["$OPTARG"]"
             if [ -z $OPTARG ]; then
                 VAGRANT_PROFILE=""
             else
@@ -156,7 +153,7 @@ while getopts m:p:t: FLAG; do
             fi
         ;;
         t)
-            echo "Setting the test manifests to ["$OPTARG"]"
+            echo -e $PREFIX"Setting the test manifests to ["$OPTARG"]"
             if [ -z $OPTARG ];then
                 TEST_MANIFESTS=""
             else
@@ -164,26 +161,26 @@ while getopts m:p:t: FLAG; do
             fi
         ;;
         \?)
-            echo "Unsupported parameter :$OPTARG"
+            echo "Unsupported option and/or parameter :"$OPTARG
+            echo "Supported parameters are:"
+            echo "[-m module_name] a list of module names, reflecting the directory naming of the module"
+            echo "[-p os_profile] a list of os profiles, reflecting the name of the vagrant defined profile"
+            echo "[-t test_manifest] a list of test manifests, reflecting the file name of the test manifests"
+            exit -1
         ;;
     esac
 done
-# -m <modulename> or -p "<modulename1 modulename2>" the name of the module to run against, this is relative to the execution location of the script, optional argument, defaults to pwd (present working directory).
-# -p <vagrant profile name> the name of the vagrant profile for your virtual machine, using the define keyword in vagrant, optional argument, defaults to all VM profiles returned by the vagrant status command
-# -t <test_manifest>
-echo "Module list [${#MODULES[*]} modules]:"
+
+#echo -e $PREFIX"Module list [${#MODULES[*]} modules]:"
 
 for ((k = 0; k < "${#MODULES[*]}"; k++));
 do
     if [ -d ${MODULES[k]} ];then
-        echo "${MODULES[k]} [$VAGRANT_PROFILE] [$TEST_MANIFESTS]"
+        echo -e $PREFIX"run_module [-m ${MODULES[k]}] [$VAGRANT_PROFILE] [$TEST_MANIFESTS]"
         run_module -m ${MODULES[k]} $VAGRANT_PROFILE $TEST_MANIFESTS
-#        echo "exit status $?"
     fi
-#    run_module "tomcat"
-    echo "exiting ${MODULES[k]}"
+    echo -e $PREFIX"exiting ${MODULES[k]}"
 done
-#Get list of VMs from vagrant file that end in _test
-echo "Test results:"
+echo -e $PREFIX"Test results:"
 echo ${RESULTS_ARRAY[@]}
 
