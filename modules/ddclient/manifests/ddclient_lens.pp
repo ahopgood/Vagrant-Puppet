801ce6653d47bbd309f1bb9fe3543ad8aa1ab29c
ahopgood <alex.hopgood@gmail.com>
2017-03-03 19:52:30 +0000

    Test for DDClientConf.lns and test manifest for setting up a lens test environment.

--- modules/ddclient/manifests/ddclient_lens.pp
+++
@@ -1,8 +0,0 @@
-class ddclient_lens{
-
-  #Install augtools
-  #Link /vagrant/files/test_ddclientconf.aug
-  #To
-  #/usr/share/augeas/lenses/tests/test_ddclientconf.aug
-  
-}
---
+++ modules/ddclient/tests/ddclient_lens.pp
@@ -0,0 +1,35 @@
+
+  Package{
+    allow_virtual => false
+  }
+
+  $local_install_path = "/etc/puppet/"
+  $local_install_dir = "${local_install_path}installers/"
+  $puppet_file_dir = "modules/ddclient/"
+
+  file {
+    "${local_install_dir}":
+      path       =>  "${local_install_dir}",
+      ensure     =>  directory,
+  }
+
+  class {"augeas":}
+  ->
+  class{"ddclient":}
+  #Install augtools
+  #Link /vagrant/files/test_ddclientconf.aug
+  #To
+  #/usr/share/augeas/lenses/tests/test_ddclientconf.aug
+  #    ensure => present,
+  #    source => "puppet:///${puppet_file_dir}test_ddclientconf.aug",
+
+  file { "/usr/share/augeas/lenses/tests/":
+    ensure => directory,
+    mode => 0777,
+  }
+  
+  file {"/usr/share/augeas/lenses/tests/test_ddclientconf.aug":
+    ensure => "link",
+    target => "/vagrant/files/test_ddclientconf.aug",
+    require => File["/usr/share/augeas/lenses/tests/"]
+  }
