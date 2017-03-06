b443514f09c6b97602a5d1814e2da2210d64959c
Alexander Hopgood <alex.hopgood@gmail.com>
2017-03-06 10:30:56 +0000

    Added perl dependencies for ddclient on CentOS 7.
    This should be spun out into a separate module for perl later on.

--- modules/ddclient/manifests/init.pp
+++ modules/ddclient/manifests/init.pp
@@ -33,16 +33,16 @@ class ddclient{
     $ddclient_file = "ddclient-${major_version}.${minor_version}.${patch_version}-2.el7.noarch.rpm"
     $provider = "rpm"
 
-    exec { "perl-install":
-#      command => "rpm -e perl perl-podlators ",
-      command => "rpm -U /vagrant/files/CentOS/7/perl-5.16.3-291.el7.x86_64.rpm \ 
-      /vagrant/files/CentOS/7/perl-libs-5.16.3-291.el7.x86_64.rpm \
-      /vagrant/files/CentOS/7/perl-Time-HiRes-1.9725-3.el7.x86_64.rpm \ 
-      /vagrant/files/CentOS/7/perl-IO-Socket-SSL-1.94-5.el7.noarch.rpm \
-      /vagrant/files/CentOS/7/perl-Digest-SHA1-2.13-9.el7.x86_64.rpm",
-      path    => "/usr/bin/",
-#      before  => [Package["perl-libs"]]
-    }
+#    exec { "perl-install":
+##      command => "rpm -e perl perl-podlators ",
+#      command => "rpm -U /vagrant/files/CentOS/7/perl-5.16.3-291.el7.x86_64.rpm \
+#      /vagrant/files/CentOS/7/perl-libs-5.16.3-291.el7.x86_64.rpm \
+#      /vagrant/files/CentOS/7/perl-Time-HiRes-1.9725-3.el7.x86_64.rpm \
+#      /vagrant/files/CentOS/7/perl-IO-Socket-SSL-1.94-5.el7.noarch.rpm \
+#      /vagrant/files/CentOS/7/perl-Digest-SHA1-2.13-9.el7.x86_64.rpm",
+#      path    => "/usr/bin/",
+##      before  => [Package["perl-libs"]]
+#    }
     
 #    package {"perl-removal": 
 ##      name => "5.16.3-283.el7",
@@ -55,6 +55,7 @@ class ddclient{
     
     $perl_libs="perl-libs-5.16.3-291.el7.x86_64.rpm"
     file {"${perl_libs}":
+      ensure => present,
       source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl_libs}",
       path => "${local_install_dir}/${perl_libs}",
     }
@@ -68,59 +69,120 @@ class ddclient{
 #      before => [Package["ddclient"], Package["perl"]]
 #    }
 
-    $perl_Time_RiHes="perl-Time-HiRes-1.9725-3.el7.x86_64.rpm"
-    file {"${perl_Time_RiHes}":
-      source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl_Time_RiHes}",
-      path => "${local_install_dir}/${perl_Time_RiHes}",
+    $perl_Time_HiRes="perl-Time-HiRes-1.9725-3.el7.x86_64.rpm"
+    file {"${perl_Time_HiRes}":
+      ensure => present,
+      source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl_Time_HiRes}",
+      path => "${local_install_dir}/${perl_Time_HiRes}",
     }
 
-#    package {"perl_Time_RiHes":
-#      source => "${local_install_dir}/${perl_Time_RiHes}",
+#    package {"perl_Time_HiRes":
+#      source => "${local_install_dir}/${perl_Time_HiRes}",
 #      ensure => "1.9725-3.el7",
 #      provider => "${provider}",
-#      require => File["${perl_Time_RiHes}"],
+#      require => File["${perl_Time_HiRes}"],
 #      install_options => "--force",
 #      before => [Package["ddclient"], Package["perl"]]
 #    }
 
     $perl="perl-5.16.3-291.el7.x86_64.rpm"
     file {"${perl}":
+      ensure => present,
       source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl}",
       path => "${local_install_dir}/${perl}",
     }
     
-#    package {"perl":
-#      source => "${local_install_dir}/${perl}",
-#      ensure => "5.16.3-291.el7",
-##      ensure => installed,
-#      provider => "${provider}",
-#      require => File["${perl}"],
-#      install_options => "--force",
-#      before => Package["ddclient"]
-#    }
+    package {["perl","perl-libs","perl_Time_HiRes"]:
+      source => ["${local_install_dir}/${perl}","${local_install_dir}/${perl_libs}","${local_install_dir}/${perl_Time_HiRes}"],
+      ensure => ["5.16.3-291.el7","5.16.3-291.el7","1.9725-3.el7"],
+#      ensure => installed,
+      provider => "${provider}",
+      require => [File["${perl}"],File["${perl_libs}"], File["${perl_Time_HiRes}"]],
+      install_options => "--force",
+      before => Package["ddclient"]
+    }
     
     $perl_digest="perl-Digest-SHA1-2.13-9.el7.x86_64.rpm"
     file {"${perl_digest}":
+      ensure => present,
       source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl_digest}",
       path => "${local_install_dir}/${perl_digest}",
     }
 
-    
-    $perl_IO_socket="perl-IO-Socket-SSL-1.94-5.el7.noarch.rpm"
-    file {"${perl_IO_socket}":
-      source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl_IO_socket}",
-      path => "${local_install_dir}/${perl_IO_socket}",
+
+    package {"perl-Digest-SHA1":
+      source => "${local_install_dir}/${perl_digest}",
+      ensure => "2.13-9.el7",
+      provider => "${provider}",
+      require => File["${perl_digest}"],
+      before => Package["ddclient"],
+    }
+
+    #Requires:
+    #perl(IO::Socket::IP) >= 0.20 is needed by perl-IO-Socket-SSL-1.94-5.el7.noarch
+    #   provider: perl-IO-Socket-IP.noarch 0.21-4.el7
+
+    $perl_IO_Socket_IP = "perl-IO-Socket-IP-0.21-4.el7.noarch.rpm"
+    file {"${perl_IO_Socket_IP}":
+      ensure => present,
+      path => "${local_install_dir}/${perl_IO_Socket_IP}",
+      source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl_IO_Socket_IP}",
+    }
+
+    package {"perl-IO-Socket-IP":
+      ensure => "0.21-4.el7",
+      provider => "${provider}",
+      source => "${local_install_dir}/${perl_IO_Socket_IP}",
+      require => File["${perl_IO_Socket_IP}"],
+    }
+
+    #perl(Net::LibIDN) is needed by perl-IO-Socket-SSL-1.94-5.el7.noarch
+    #   provider: perl-Net-LibIDN.x86_64 0.12-15.el7
+    $perl_Net_LibIDN = "perl-Net-LibIDN-0.12-15.el7.x86_64.rpm"
+    file {"${perl_Net_LibIDN}":
+      ensure => present,
+      path => "${local_install_dir}/${perl_Net_LibIDN}",
+      source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl_Net_LibIDN}",
+    }
+    package{"perl-Net-LibIDN":
+      ensure => "0.12-15.el7",
+      provider => "${provider}",
+      source => "${local_install_dir}/${perl_Net_LibIDN}",
+      require => File["${perl_Net_LibIDN}"]
+    }
+
+
+    #perl(Net::SSLeay) is needed by perl-IO-Socket-SSL-1.94-5.el7.noarch
+    #perl(Net::SSLeay) >= 1.21 is needed by perl-IO-Socket-SSL-1.94-5.el7.noarch
+    #    provider: perl-Net-SSLeay.x86_64 1.55-3.el7
+    $perl_Net_SSLeay = "perl-Net-SSLeay-1.55-4.el7.x86_64.rpm"
+    file {"${perl_Net_SSLeay}":
+      ensure => present,
+      path => "${local_install_dir}/${perl_Net_SSLeay}",
+      source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl_Net_SSLeay}",
+    }
+    package{"perl-Net-SSLeay":
+      ensure => "1.55-4.el7",
+      provider => "${provider}",
+      source => "${local_install_dir}/${perl_Net_SSLeay}",
+      require => File["${perl_Net_SSLeay}"],
+    }
+
+    $perl_IO_Socket_SSL="perl-IO-Socket-SSL-1.94-5.el7.noarch.rpm"
+    file {"${perl_IO_Socket_SSL}":
+      ensure => present,
+      source => "puppet:///${puppet_file_dir}${OS}/${OS_version}/${perl_IO_Socket_SSL}",
+      path => "${local_install_dir}/${perl_IO_Socket_SSL}",
     }
     
-#    package {"perl":
-#      source => "${local_install_dir}/${perl}",
-#      ensure => "5.16.3-291.el7",
-##      ensure => installed,
-#      provider => "${provider}",
-#      require => File["${perl}"],
+    package { "perl-IO-Socket-SSL":
+      source          => "${local_install_dir}/${perl_IO_Socket_SSL}",
+      ensure          => "1.94-5.el7",
+      provider        => "${provider}",
+      require         => [File["${perl_IO_Socket_SSL}"], Package["perl-IO-Socket-IP"], Package["perl-Net-LibIDN"], Package["perl-Net-SSLeay"] ],
 #      install_options => "--force",
-#      before => Package["ddclient"]
-#    
+      before          => Package["ddclient"],
+    }
   }
     file{"${ddclient_file}":
       ensure => present,
