0b7c53b0fdd8adf9a3facf46589ae6b348bbce7e
Alexander Hopgood <alex.hopgood@gmail.com>
2017-03-09 15:02:38 +0000

    Added test where document root is missing on virtual host.

---
+++ modules/httpd/tests/virtual_host_httpd_no_document_root.pp
@@ -0,0 +1,24 @@
+Package{
+  allow_virtual => false
+}
+
+  $local_install_path = "/etc/puppet/"
+  $local_install_dir = "${local_install_path}installers/"
+
+  file {
+    "${local_install_dir}":
+    path       =>  "${local_install_dir}",
+    ensure     =>  directory,
+  } 
+
+  class { "iptables":
+    port => "80",
+  }
+  
+  class { "httpd": }
+  
+  class { "augeas": }
+
+  httpd::virtual_host{"test":
+    server_name => "www.alexander.com",
+  }
