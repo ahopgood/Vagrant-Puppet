7f08efb22ddb9e67bdb274f00d3b7b1c099d4391
ahopgood <alex.hopgood@gmail.com>
2017-04-08 10:17:52 +0100

    Moved php.pp test file to the tests folder to enable use of the test_harness.sh

---
+++ modules/php/tests/php.pp
@@ -0,0 +1,17 @@
+Package{
+  allow_virtual => false
+}
+  $local_install_path = "/etc/puppet/"
+  $local_install_dir = "${local_install_path}installers/"
+
+
+  file {
+    "${local_install_dir}":
+    path       =>  "${local_install_dir}",
+    ensure     =>  directory,
+  } 
+
+  class {"mysql":}
+  class {"iptables": port => "80"}
+  class {"httpd":}
+  class {"php":}
