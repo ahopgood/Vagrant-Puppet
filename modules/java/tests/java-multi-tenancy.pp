65a0cd37933294c8876fafc43216879014a25df9
Alexander Hopgood <alex.hopgood@gmail.com>
2017-04-19 10:27:34 +0100

    Updated java-multi-tenancy test to use major_version instead of version parameter.

--- modules/java/tests/java-multi-tenancy.pp
+++ modules/java/tests/java-multi-tenancy.pp
@@ -2,14 +2,14 @@ Package{
   allow_virtual => false,
 }
 java{"java-6":
-  version => "6",
+  major_version => "6",
   update_version => "45",
   multiTenancy => true,
   isDefault => true,
 }
 
 java{"java-7":
-  version => '7',
+  major_version => '7',
   update_version => '76',
   multiTenancy => true,
 }
@@ -17,5 +17,4 @@ java{"java-7":
 java::jce {"jce8":
   major_version => "8",
   update_version => "31",
-  multiTenancy => true,
 }
