fa0eaa9984eb1bd86d7b09011acc1d186005733e
Alexander Hopgood <alex.hopgood@gmail.com>
2017-05-01 18:51:33 +0100

    Test for apache csp header

---
+++ modules/httpd/tests/virtual_host_httpd_csp.pp
@@ -0,0 +1,33 @@
+Package{
+  allow_virtual => false
+}
+
+  $local_install_path = "/etc/puppet/"
+  $local_install_dir = "${local_install_path}installers/"
+
+  file {
+    "${local_install_dir}":
+    path       =>  "${local_install_dir}",
+    ensure     =>  directory,
+  }
+  class { "httpd": }
+
+  httpd::virtual_host{"test":
+    server_name => "www.alexander.com",
+    document_root => "/var/www/alexander/",
+    server_alias => ["alexander.com","alexander.net"]
+  }
+  ->
+  file {"/var/www/alexander/":
+    ensure => directory,
+    #    require => Class["httpd"]
+  }
+  ->
+  file {"/var/www/alexander/index.html":
+    ensure => present,
+    content => "<html><title>Test Page</title><body><h1>Alex's test page</h1></body></html>"
+  }
+  ->
+  httpd::content_security_policy{"CSP":
+    
+  }
