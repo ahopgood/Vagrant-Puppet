fff4df49e587eb36831e8123d7d90a662028d8f9
Alex Hopgood <alex.hopgood@gmail.com>
2017-05-15 20:33:46 +0100

    Updated x-clacks module to make use of header::install definition

--- modules/httpd/manifests/xclacks.pp
+++ modules/httpd/manifests/xclacks.pp
@@ -28,19 +28,20 @@ define httpd::xclacks {
 #    notify{"Trying to set apache file":}
 # set /files/etc/apache2/apache2.conf/Directory[3]/arg "/var/www/html"
 
+  httpd::header::install{"x-clacks":}
   if ("${operatingsystem}" == "Ubuntu"){
-    exec {"enable headers plugin": 
-      path => "/usr/sbin/:/usr/bin/",
-      command => "/usr/sbin/a2enmod headers",
-      unless => "/bin/ls -l /etc/apache2/mods-enabled/ | /bin/grep headers",
-      require => Package["apache2"],
-    }
-    ->
-    exec {"restart-apache2-to-install-headers":
-      path => "/usr/sbin/:/bin/",
-      command => "service apache2 restart",
-    }
-    
+#    exec {"enable headers plugin": 
+#      path => "/usr/sbin/:/usr/bin/",
+#      command => "/usr/sbin/a2enmod headers",
+#      unless => "/bin/ls -l /etc/apache2/mods-enabled/ | /bin/grep headers",
+#      require => Package["apache2"],
+#    }
+#    ->
+#    exec {"restart-apache2-to-install-headers":
+#      path => "/usr/sbin/:/bin/",
+#      command => "service apache2 restart",
+#    }
+#    
     $header_contents = [
     "ins Directory after /files/etc/apache2/apache2.conf/Directory[last()]",
     "set Directory[last()]/arg '\"/var/www/html/\"'",
@@ -69,34 +70,34 @@ define httpd::xclacks {
       #Find if the module is installed
       #apachectl -t -D DUMP_MODULES | grep headers
       
-      exec {"module_source_exists":
-        path => "/bin/",
-        command => "ls -l /etc/httpd/modules/ | grep headers",
-      }
-      #For the following line:
-      #LoadModule headers_module modules/mod_headers.so
-      $module_load_contents = [
-        "ins directive after /files/etc/httpd/conf/httpd.conf/directive[last()]",
-        "set directive[last()] LoadModule",
-        "set directive[last()]/arg[1] headers_module",
-        "set directive[last()]/arg[2] modules/mod_headers.so",
-      ]  
-    
-      augeas {"add header module to httpd config":
-        incl => "/etc/httpd/conf/httpd.conf",
-        lens => "Httpd.lns",
-        context => "/files/etc/httpd/conf/httpd.conf/",
-        changes => $module_load_contents,
-        onlyif   => "match /files/etc/httpd/conf/httpd.conf/directive[. = 'LoadModule']/arg[. = 'headers_module'] size == 0",
-        require => Exec["module_source_exists"],
-      }
-      ->
-      exec {"restart-httpd-to-add-headers-module":
-        path => "/sbin/:/bin/",
-        command => "service httpd reload",
-        unless => "/usr/sbin/apachectl -t -D DUMP_MODULES | /bin/grep headers",
-        before => Augeas["add header to directory"],
-      }
+#      exec {"module_source_exists":
+#        path => "/bin/",
+#        command => "ls -l /etc/httpd/modules/ | grep headers",
+#      }
+#      #For the following line:
+#      #LoadModule headers_module modules/mod_headers.so
+#      $module_load_contents = [
+#        "ins directive after /files/etc/httpd/conf/httpd.conf/directive[last()]",
+#        "set directive[last()] LoadModule",
+#        "set directive[last()]/arg[1] headers_module",
+#        "set directive[last()]/arg[2] modules/mod_headers.so",
+#      ]  
+#    
+#      augeas {"add header module to httpd config":
+#        incl => "/etc/httpd/conf/httpd.conf",
+#        lens => "Httpd.lns",
+#        context => "/files/etc/httpd/conf/httpd.conf/",
+#        changes => $module_load_contents,
+#        onlyif   => "match /files/etc/httpd/conf/httpd.conf/directive[. = 'LoadModule']/arg[. = 'headers_module'] size == 0",
+#        require => Exec["module_source_exists"],
+#      }
+#      ->
+#      exec {"restart-httpd-to-add-headers-module":
+#        path => "/sbin/:/bin/",
+#        command => "service httpd reload",
+#        unless => "/usr/sbin/apachectl -t -D DUMP_MODULES | /bin/grep headers",
+#        before => Augeas["add header to directory"],
+#      }
       
       #module check
       #<IfModule mod_headers.c>
