74d6fa71e4f5934e85c514680904c5af9d9cfa80
Alex Hopgood <alex.hopgood@gmail.com>
2017-05-18 16:11:49 +0100

    X-Clacks-Overhead refactored again to bring the global bit inside the global conditional. Old code that was previously commented out has been removed.

--- modules/httpd/manifests/xclacks.pp
+++ modules/httpd/manifests/xclacks.pp
@@ -1,108 +1,15 @@
 define httpd::xclacks(
   $virtual_host = "global"
 ) {
-/*
-  Using augeas we need to add the following to a .htaccess files
-  <IfModule headers_module>
-    header set X-Clacks-Overhead "GNU Terry Pratchett"
-  </IfModule>
-
- We need to create a .htaccess file.
-  
- After investigating .htaccess files a bit it seems they are frowned upon and are not enabled by default in apache versions 2.3.9 and later.
- Instead we should add the text to the httpd.conf file inside a Directory tag.
- <Directory "/var/www/html/">
-  <IfModule headers_module>
-    header set X-Clacks-Overhead "GNU Terry Pratchett"
-  </IfModule>
- </Directory>
-
-  We need to also enable the headers_module in apache by using a2enmod followed by the input headers
-  sudo a2enmod --force headers
-  This will prevent it asking for input
-  This actually creates the following link in /etc/apache/mods-enabled/
-  lrwxrwxrwx 1 root root 30 Dec 30 15:44 headers.load -> ../mods-available/headers.load
-  Some plugins require a .conf file as well.
-  Then we need to restart the apache service (varies based on os)
-
-*/
-
-#    notify{"Trying to set apache file":}
-# set /files/etc/apache2/apache2.conf/Directory[3]/arg "/var/www/html"
 
   httpd::header::install{"x-clacks":}
-  $header_value = "\"\\\"GNU Terry Pratchett\"\\\""
-  httpd::header::set_global{
-    "x-clacks":
-      header_name => "X-Clacks-Overhead",
-      header_value => $header_value,
-  }
   if (versioncmp("$virtual_host", "global") == 0){
-#    if ("${operatingsystem}" == "Ubuntu"){
-#      $header_contents = [
-#        "ins Directory after /files/etc/apache2/apache2.conf/Directory[last()]",
-#        "set Directory[last()]/arg '\"/var/www/html/\"'",
-#        "set Directory[last()]/IfModule/arg headers_module",
-#        "set Directory[last()]/IfModule/directive[1] header",
-#        "set Directory[last()]/IfModule/directive[1]/arg[1] set",
-#        "set Directory[last()]/IfModule/directive[1]/arg[2] X-Clacks-Overhead",
-#        "set Directory[last()]/IfModule/directive[1]/arg[3] '\"GNU Terry Pratchett\"'",
-#      ]
-#      augeas {"add header to directory":
-#        incl => "/etc/apache2/apache2.conf",
-#        lens => "Httpd.lns",
-#        context => "/files/etc/apache2/apache2.conf/",
-#        changes => $header_contents,
-#        require => Httpd::Header::Install["x-clacks"],
-#        onlyif   => "match /files/etc/apache2/apache2.conf/Directory[.]/IfModule[.]/directive[. = 'header']/arg[. = 'X-Clacks-Overhead'] size == 0",
-#      }
-#      ->
-#      exec {"restart-apache2-to-add-x-clacks":
-#        path => "/usr/sbin/:/bin/",
-#        command => "service apache2 reload",
-#      }
-#    } elsif ("${operatingsystem}" == "CentOS"){
-#      if ("${operatingsystemmajrelease}" == "6" or "${operatingsystemmajrelease}" == "7") {
-#        notify{"Beginning support for CentOS${operatingsystemmajrelease}":}
-#        #module check
-#        #<IfModule mod_headers.c>
-#        $header_contents = [
-#          "ins IfModule after /files/etc/httpd/conf/httpd.conf/IfModule[last()]",
-#          "set IfModule[last()]/arg mod_headers.c",
-#          "set IfModule[last()]/Directory/arg '\"/var/www/html/\"'",
-#          "set IfModule[last()]/Directory/directive[1] header",
-#          "set IfModule[last()]/Directory/directive[1]/arg[1] set",
-#          "set IfModule[last()]/Directory/directive[1]/arg[2] X-Clacks-Overhead",
-#          "set IfModule[last()]/Directory/directive[1]/arg[3] '\"GNU Terry Pratchett\"'",
-#        ]
-#        augeas {"add header to directory":
-#          incl => "/etc/httpd/conf/httpd.conf",
-#          lens => "Httpd.lns",
-#          context => "/files/etc/httpd/conf/httpd.conf/",
-#          changes => $header_contents,
-#          onlyif   => "match /files/etc/httpd/conf/httpd.conf/Directory[.]/directive[. = 'header']/arg[. = 'X-Clacks-Overhead'] size == 0",
-#        }
-#        ->
-#        exec {"restart-httpd-to-add-x-clacks":
-#          path => "/sbin/:/bin/",
-#          command => "service httpd reload",
-#        }
-#      }#close centos 6 & 7 check
-#      #Tells us if the module is installed, ideally want to know when it isn't install 
-#      #get /files/etc/httpd/conf/httpd.conf/directive[. = 'LoadModule']/arg[. != 'headers_module']
-#
-#      #Match all headers_module
-#      #match /files/etc/httpd/conf/httpd.conf/directive[. = 'LoadModule']/arg[. == 'headers_module']
-#      #Match all non headers_module
-#      #match /files/etc/httpd/conf/httpd.conf/directive[. = 'LoadModule']/arg[. != 'headers_module']
-#
-#      #match /files/etc/httpd/conf/httpd.conf/directive[. = 'LoadModule']/arg[1] != 'headers_module'
-#      #match directive[. = '/LoadModule'] size == 0
-#    } else {
-#      fail("${operatingsystem} is not currently supported")
-#    }
-    #apache2ctl -M | grep headers
-    #ls -l /etc/apache2/mods-enabled/ | grep headers
+    $header_value = "\"\\\"GNU Terry Pratchett\"\\\""
+    httpd::header::set_global{
+      "x-clacks":
+        header_name => "X-Clacks-Overhead",
+        header_value => $header_value,
+    }
   } else {
     $header_value = "\"\\\"GNU Terry Pratchett\"\\\""
     httpd::header::set{
