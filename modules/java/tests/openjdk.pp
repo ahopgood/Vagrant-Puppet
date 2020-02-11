f9b2e917fcaebcc3b905ddcd1df210a56fb8a07f
Alexander Hopgood <alex.hopgood@gmail.com>
2020-02-11 21:03:55 +0000

    OpenJdk class/module for AdoptOpenJdk Java distro.

---
+++ modules/java/tests/openjdk.pp
@@ -0,0 +1,63 @@
+Package{
+  allow_virtual => false,
+}
+$local_install_path = "/etc/puppet/"
+$local_install_dir = "${local_install_path}installers/"
+$puppet_file_dir = "modules/java/"
+
+file {
+  "${local_install_dir}":
+    path       =>  "${local_install_dir}",
+    ensure     =>  directory,
+}
+
+# java::openjdk::ubuntu::xenial {"java8":
+#   major_version => "8",
+#   update_version => "232",
+#   # isDefault => true,
+#   # multiTenancy => false,
+#   multiTenancy => true,
+# }
+# ->
+# java::openjdk::ubuntu::xenial {"java11":
+#   major_version => "11",
+#   update_version => "5",
+#   # isDefault => true,
+#   # multiTenancy => false,
+#   multiTenancy => true,
+# }
+
+# java::ubuntu{"AdoptOpenJdk test via the Java::Ubuntu resource":
+#   major_version => "8",
+#   update_version => "232",
+#   multiTenancy => true
+# }
+# sudo puppet apply --parser=future /vagrant/tests/openjdk.pp
+# java {"Oracle test via the Java resource":
+#   major_version => "8",
+#   update_version => "212",
+#   multiTenancy => false,
+#   isDefault => false,
+# }
+# ->
+java {"AdoptOpenJdk test via the Java resource":
+  major_version => "8",
+  update_version => "232",
+  multiTenancy => false,
+  isDefault => true,
+}
+# ->
+# java {"AdoptOpenJdk 11 test via the Java resource":
+#   major_version => "11",
+#   update_version => "5",
+#   multiTenancy => true,
+#   isDefault => false,
+# }
+# ->
+# java::openjdk::ubuntu::create_default{"test-11-create":
+#   major_version => "8",
+# }
+# ->
+# java::openjdk::ubuntu::set_default{"test-11-set":
+#   major_version => "11",
+# }
